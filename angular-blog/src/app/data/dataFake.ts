9761d4204c0c94653448cb83a814a25b5bafa41a
MAXIMUS ROSAS BURGOS <lianthg07@gmail.com>
2025-03-15 07:27:18 -0300

    Commit do Blog Angular

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Sonic vs. Shadow: O Confronto Épico Chega às Telonas",
+    "description": "No terceiro filme da franquia, Sonic, Tails e Knuckles unem forças para enfrentar Shadow, um ouriço criado pelo avô do Dr. Robotnik, Gerald Robotnik, que busca vingança contra a humanidade. Dirigido por Jeff Fowler, o filme conta com a volta de Jim Carrey como Dr. Robotnik e a estreia de Keanu Reeves como a voz de Shadow. A trama explora a origem de Shadow, mantendo elementos dos videogames, e introduz criaturas como os Chao. Lançado em 25 de dezembro de 2024 no Brasil, o filme recebeu críticas positivas, destacando as performances de Carrey e Reeves. Com um orçamento de US$ 122 milhões, arrecadou US$ 486 milhões mundialmente, tornando-se o filme de maior bilheteria da franquia e o segundo filme baseado em videogame de maior bilheteria de todos os tempos. Uma sequência já está em desenvolvimento, com lançamento previsto para 19 de março de 2027.",
+    "photoCover":"https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/08/sonic-3-filme-e1724765794391.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Surgem Rumores de Possível Série Solo do Shadow na Paramount+",
+    "description": "Após a introdução de Shadow em 'Sonic 3', especulações sobre uma série solo do personagem na Paramount têm ganhado força. Alguns fãs acreditam que, devido à popularidade de Shadow, a Paramount possa estar considerando um spin-off centrado nele. Entretanto, até o momento, não há confirmações oficiais sobre o desenvolvimento de uma série dedicada ao personagem.",
+    "photoCover":"https://brasil.perfil.com/wp-content/uploads/2024/11/Sonic-3-trailer-destaque-em-Shadow.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Knuckles Ganha Série Solo na Paramount+ que tem Conexão a Sonic 3",
+    "description": "A série 'Knuckles' estreou no Paramount+ como um spin-off de 'Sonic 2: O Filme', centrando-se no personagem Knuckles, dublado por Idris Elba. Situada entre os eventos de 'Sonic 2' e 'Sonic 3', a trama acompanha Knuckles enquanto ele treina Wade Whipple, interpretado por Adam Pally, nos caminhos dos guerreiros Echidna. A série quebrou recordes de audiência na plataforma, tornando-se a maior estreia de uma produção original do serviço de streaming. Além de Elba e Pally, o elenco conta com Tika Sumpter como Maddie e outros atores renomados. A produção recebeu elogios por expandir o universo de Sonic e aprofundar a história de Knuckles.",
+    "photoCover":"https://sm.ign.com/ign_br/screenshot/default/imagem-2024-04-29-165941580_cyz3.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Cena Pós-créditos de Sonic 3 Revela Novo Vilão e Primeira Aparição de Amy Rose",
+    "description": "Na cena pós-créditos de 'Sonic 3', somos apresentados a Amy Rose e Metal Sonic, ampliando o universo cinematográfico do ouriço azul. Amy Rose, conhecida por sua personalidade forte e martelo característico, surge misteriosamente, sugerindo um papel significativo nas próximas aventuras. Em seguida, Metal Sonic, a contraparte robótica de Sonic criada pelo Dr. Robotnik, é revelado, insinuando desafios futuros para o herói. Essa introdução de personagens icônicos dos jogos eletrônicos promete novas emoções e confrontos nos próximos filmes da franquia.",
+    "photoCover":"https://preview.redd.it/my-sonic-4-theory-on-amy-and-metal-sonic-v0-b4x1ddkhprge1.jpg?width=640&crop=smart&auto=webp&s=7868cfce261bd8109942a23f16d538d4c3df7a22"
+  }
+]
